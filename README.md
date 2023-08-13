94399370ca55c27be3eb5d34739f4240843a1886
Robert Edwards <rtedwards.eng@gmail.com>
2023-08-14 00:08:49 +0100

    fixes katex font color for light/dark mode; now is black when light mode and white when dark mode.  There is a problem with light mode code syntax highlighting not being used because there's seemingly no 'light mode' only 'dark mode' and regular

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ![example workflow](https://github.com/github/docs/actions/workflows/zola-deploy-action.yml/badge.svg?branch=gh-pages)
 
 Personal blog built using [Zola](https://www.getzola.org/) and themed with [Archie Zola](https://www.getzola.org/themes/archie-zola/)
+
+## Themes
+- archie-zola
+- [serene](https://www.getzola.org/themes/serene/)
+    - light / dark katex font changes
+    - comments via giscus
--- static/js/themetoggle.js
+++ static/js/themetoggle.js
@@ -1,24 +1,22 @@
 function setTheme(mode) {
-    localStorage.setItem("theme-storage", mode);
-    if (mode === "dark") {
-        document.getElementById("darkModeStyle").disabled=false;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
-        feather.replace();
-        // document.getElementById("darkModeKatex").disabled=false;
-    } else if (mode === "light") {
-        document.getElementById("darkModeStyle").disabled=true;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
-        feather.replace();
-        // document.getElementById("darkModeKatex").disabled=true;
-    }
+  localStorage.setItem("theme-storage", mode);
+  if (mode === "dark") {
+    document.getElementById("darkModeStyle").disabled = false;
+    document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
+    feather.replace()
+  } else if (mode === "light") {
+    document.getElementById("darkModeStyle").disabled = true;
+    document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
+    feather.replace()
+  }
 }
 
 function toggleTheme() {
-    if (localStorage.getItem("theme-storage") === "light") {
-        setTheme("dark");
-    } else if (localStorage.getItem("theme-storage") === "dark") {
-        setTheme("light");
-    }
+  if (localStorage.getItem("theme-storage") === "light") {
+    setTheme("dark");
+  } else if (localStorage.getItem("theme-storage") === "dark") {
+    setTheme("light");
+  }
 }
 
 var savedTheme = localStorage.getItem("theme-storage") || "light";
